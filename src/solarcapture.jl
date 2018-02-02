f038462f94ff8ccba685d0f2179ee2b99952fea8
Judd Mehr <juddmehr@gmail.com>
2018-02-01 17:40:56 -0700

    fix spacing

--- src/solarcapture.jl
+++ src/solarcapture.jl
@@ -1,66 +1,66 @@
 """
     solarcapturesimple(solardata::sunshine, trajectory::aircrafttrajectory,
-	  solarpanels::panelgeometry,etasolar::Float64)
+      solarpanels::panelgeometry,etasolar::Float64)
 
 Calculate the flux, power, and total energy for all input panels for all input time.
 """
 function solarcapturesimple(solardata::sunshine,trajectory::aircrafttrajectory,
-	solarpanels::Array{panelgeometry,1},etasolar::Float64)
+    solarpanels::Array{panelgeometry,1},etasolar::Float64)
 
-	#initialize
-	panelflux = Array{Float64,2}(length(solarpanels),length(solardata.time))
-	panelpower = Array{Float64,2}(length(solarpanels),length(solardata.time))
-	paneltotalenergy = Array{Float64,2}(length(solarpanels),length(solardata.time))
+    #initialize
+    panelflux = Array{Float64,2}(length(solarpanels),length(solardata.time))
+    panelpower = Array{Float64,2}(length(solarpanels),length(solardata.time))
+    paneltotalenergy = Array{Float64,2}(length(solarpanels),length(solardata.time))
 
-	for i=1:length(solardata.time)-1
+    for i=1:length(solardata.time)-1
 
-		# --- Get Normalized Solar Vector --- #
-		A = -solardata.azimuth[i] + 90.0*pi/180.0 #Correct azimuth angle orientation (x-axis = 0 , positive counter-clockwise)
-		Z = solardata.zenith[i]
+        # --- Get Normalized Solar Vector --- #
+        A = -solardata.azimuth[i] + 90.0*pi/180.0 #Correct azimuth angle orientation (x-axis = 0 , positive counter-clockwise)
+        Z = solardata.zenith[i]
 
-		# Convert sun angles to vector
-		u = cos(A).*sin(Z)
-		v = sin(A).*sin(Z)
-		w = cos(Z)
+        # Convert sun angles to vector
+        u = cos(A).*sin(Z)
+        v = sin(A).*sin(Z)
+        w = cos(Z)
 
-		sunvector = [u,v,w]
-		# ---
+        sunvector = [u,v,w]
+        # ---
 
-		for j=1:length(solarpanels)
+        for j=1:length(solarpanels)
 
-			# --- Adjust panel normal by aircraft orientation --- #
-			# complete trig calculations for brevity
-			cphi = cos(-trajectory.roll[i])
-			cth = cos(-trajectory.pitch[i])
-			cpsi = cos(trajectory.yaw[i])
-			sphi = sin(-trajectory.roll[i])
-			sth = sin(-trajectory.pitch[i])
-			spsi = sin(trajectory.yaw[i])
+            # --- Adjust panel normal by aircraft orientation --- #
+            # complete trig calculations for brevity
+            cphi = cos(-trajectory.roll[i])
+            cth = cos(-trajectory.pitch[i])
+            cpsi = cos(trajectory.yaw[i])
+            sphi = sin(-trajectory.roll[i])
+            sth = sin(-trajectory.pitch[i])
+            spsi = sin(trajectory.yaw[i])
 
-			# 3D Rotation Matrix
-			R = [cth*cpsi -cphi*spsi+sphi*sth*cpsi   sphi*spsi+cphi*sth*cpsi;
-				   cth*spsi  cphi*cpsi+sphi*sth*spsi  -sphi*cpsi+cphi*sth*spsi;
-				  -sth 	     sphi*cth 				         cphi*cth               ]
+            # 3D Rotation Matrix
+            R = [cth*cpsi -cphi*spsi+sphi*sth*cpsi   sphi*spsi+cphi*sth*cpsi;
+                   cth*spsi  cphi*cpsi+sphi*sth*spsi  -sphi*cpsi+cphi*sth*spsi;
+                  -sth          sphi*cth                          cphi*cth               ]
 
-			panelnormal = R*solarpanels[j].normal
+            panelnormal = R*solarpanels[j].normal
 
-			# --- Get obliquity factor (0-1) --- #
-			mu = sum(sunvector.*panelnormal)
-			if mu < 0.0
-				mu = 0.0
-			end
+            # --- Get obliquity factor (0-1) --- #
+            mu = sum(sunvector.*panelnormal)
+            if mu < 0.0
+                mu = 0.0
+            end
 
-			# --- Calculate Flux, Power, and Total Energy --- #
-			panelflux[j,i] = solardata.flux[i].*mu
+            # --- Calculate Flux, Power, and Total Energy --- #
+            panelflux[j,i] = solardata.flux[i].*mu
 
-			panelpower[j,i] = etasolar*solarpanels[j].area*panelflux[j,i]
+            panelpower[j,i] = etasolar*solarpanels[j].area*panelflux[j,i]
 
-			paneltotalenergy[j,i] = panelpower[j,i]*(solardata.time[i+1]-solardata.time[i])
+            paneltotalenergy[j,i] = panelpower[j,i]*(solardata.time[i+1]-solardata.time[i])
 
-		end #for number of panels
-	end #for all solardata.time
+        end #for number of panels
+    end #for all solardata.time
 
-	return panelenergy(panelflux,panelpower,paneltotalenergy)
+    return panelenergy(panelflux,panelpower,paneltotalenergy)
 
 end #solarcapturesimple()
 
@@ -81,7 +81,7 @@ function getsunshine(file::String)
   azimuth = data[:,9]*pi/180 # azimuth angle of solar flux
   zenith = data[:,10]*pi/180 # zenith angle of solar flux
 
-	return sunshine(time,azimuth,zenith,flux)
+    return sunshine(time,azimuth,zenith,flux)
 end
 
 """
@@ -91,6 +91,6 @@ Interpolates solardata struct data to fit a new array of time values.
 function interpolatesunshine(solardata::sunshine,time::Array{Float64,1})
   azimuthitp = interpolate((solardata.time,),solardata.azimuth,Gridded(Linear()))
   zenithitp = interpolate((solardata.time,),solardata.zenith,Gridded(Linear()))
-	fluxitp = interpolate((solardata.time,),solardata.flux,Gridded(Linear()))
-	return sunshine(time,azimuthitp[time],zenithitp[time],fluxitp[time])
+    fluxitp = interpolate((solardata.time,),solardata.flux,Gridded(Linear()))
+    return sunshine(time,azimuthitp[time],zenithitp[time],fluxitp[time])
 end
--- src/solartypes.jl
+++ src/solartypes.jl
@@ -5,89 +5,89 @@
 Holds aircraft orientation information for each time step
 """
 struct aircrafttrajectory
-	roll::Array{Float64,1}
-	pitch::Array{Float64,1}
-	yaw::Array{Float64,1}
+    roll::Array{Float64,1}
+    pitch::Array{Float64,1}
+    yaw::Array{Float64,1}
 end
 
 # ----- Solar Panel Types ----- #
 "
-	panelgeometry
+    panelgeometry
 
 Describe the geometry/orientation of a solar panel array.
 "
 struct panelgeometry
-	normal::Array{Float64,1}
-	chord::Float64
-	span::Float64
-	area::Float64
-	roll::Float64
-	pitch::Float64
-	yaw::Float64
+    normal::Array{Float64,1}
+    chord::Float64
+    span::Float64
+    area::Float64
+    roll::Float64
+    pitch::Float64
+    yaw::Float64
 end
 
 """
-	panelgeometry(;chord = 1.0, span = 1.0, roll = 0.0, pitch = 0.0, yaw = 0.0))
+    panelgeometry(;chord = 1.0, span = 1.0, roll = 0.0, pitch = 0.0, yaw = 0.0))
 
 Define the panelgeometry immutable type; calculate and fill in the area and normal fields from the required inputs for the user.
 
 See documentation for the panelgeometry immutable type for information on inputs.
 """
 function panelgeometry( #TODO find dimensions of single solar cell and put those as defaults
-	chord = 1.0,
-	span  = 1.0,
-	roll  = 0.0,
-	pitch = 0.0,
-	yaw   = 0.0)
+    chord = 1.0,
+    span  = 1.0,
+    roll  = 0.0,
+    pitch = 0.0,
+    yaw   = 0.0)
 
-	area = chord*span
+    area = chord*span
 
-	# Calculate panel normal in inertial frame
-	c1 = cos(-roll)
-	c2 = cos(-pitch)
-	c3 = cos(yaw)
-	s1 = sin(-roll)
-	s2 = sin(-pitch)
-	s3 = sin(yaw)
+    # Calculate panel normal in inertial frame
+    c1 = cos(-roll)
+    c2 = cos(-pitch)
+    c3 = cos(yaw)
+    s1 = sin(-roll)
+    s2 = sin(-pitch)
+    s3 = sin(yaw)
 
-	n1 = c1.*s2.*s3 - c3.*s1
-	n2 = c1.*c3.*s2 + s1.*s3
-	n3 = c1.*c2
+    n1 = c1.*s2.*s3 - c3.*s1
+    n2 = c1.*c3.*s2 + s1.*s3
+    n3 = c1.*c2
 
-	normal = [n1, n2, n3]
+    normal = [n1, n2, n3]
 
-	return panelgeometry(normal, chord, span, area, roll, pitch, yaw)
+    return panelgeometry(normal, chord, span, area, roll, pitch, yaw)
 
 end
 
 """
-	panelparameters
+    panelparameters
 
 """
 struct panelparameters
-	#TODO talk to Nathaniel about what needs to go in here as far as the power stuff is concerned
+    #TODO talk to Nathaniel about what needs to go in here as far as the power stuff is concerned
 end #panelphysicsin
 
 """
-	panelenergy
+    panelenergy
 
 Define flux, power, and total energy arrays for a solar panel network.
 """
 struct panelenergy
-	flux::Array{Float64,2}
-	power::Array{Float64,2}
-	totalenergy::Array{Float64,2}
+    flux::Array{Float64,2}
+    power::Array{Float64,2}
+    totalenergy::Array{Float64,2}
 end #panelphysicsout
 
 # ----- Sunshine Types ----- #
 """
-	sunshine
+    sunshine
 
 Define time, sun angles (azimuth, zenith), and solar flux arrays.
 """
 struct sunshine
-	time::Array{Float64,1}
-	azimuth::Array{Float64,1}
-	zenith::Array{Float64,1}
-	flux::Array{Float64,1}
+    time::Array{Float64,1}
+    azimuth::Array{Float64,1}
+    zenith::Array{Float64,1}
+    flux::Array{Float64,1}
 end
